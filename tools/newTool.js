648ff9c8306f84287bbc4f2f50bee40f6849feae
abdomody35 <abdomody35@gmail.com>
2024-08-22 18:42:40 +0300

    fixed an issue with the tool

--- tools/newTool.js
+++ tools/newTool.js
@@ -31,7 +31,12 @@ const CRAWL_USING_REGEX = {
         name
       )}`;
 
-      const googleResponse = await axios.get(googleSearchUrl);
+      const googleResponse = await axios.get(googleSearchUrl, {
+        headers: {
+          "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        },
+      });
 
       const $ = cheerio.load(googleResponse.data);
       const allLinks = $("#search a")
